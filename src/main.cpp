5ecd60f68fcd2eab44524ac6b685f14c91f95ad7
alex nakagama <alexnakagama4@gmail.com>
2026-05-28 18:47:13 -0300

    added prints

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
+#include "HardwareSerial.h"
 #include "esp32-hal-gpio.h"
 #include "esp32-hal.h"
 #include <Arduino.h>
 
 #define LED_PIN 2
 
-void setup() { pinMode(LED_PIN, OUTPUT); }
+void setup() { pinMode(LED_BUILTIN, OUTPUT); }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("LED on");
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LED off");
+    delay(1000);
 }
